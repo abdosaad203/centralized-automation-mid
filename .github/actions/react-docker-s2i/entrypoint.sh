ab62b227cc282ee21edd40e337a4f7c7127ff0d8
abdosaad203 <abdosaad4900@gmail.com>
2026-01-13 02:51:11 +0200

    Fix React S2I action: support VITE_API_BASE_URL

--- .github/actions/react-docker-s2i/entrypoint.sh
+++ .github/actions/react-docker-s2i/entrypoint.sh
@@ -2,15 +2,17 @@
 set -e
 
 echo "Logging in to registry..."
-echo "$INPUT_PASSWORD" | docker login $INPUT_REGISTRY \
+echo "$INPUT_PASSWORD" | docker login "$INPUT_REGISTRY" \
   -u "$INPUT_USERNAME" --password-stdin
 
 IMAGE="$INPUT_REGISTRY/$INPUT_IMAGE_NAME:$INPUT_TAG"
 
 echo "Building Docker image..."
-docker build -t $IMAGE .
+docker build \
+  --build-arg VITE_API_BASE_URL="$INPUT_API_BASE_URL" \
+  -t "$IMAGE" .
 
 echo "Pushing Docker image..."
-docker push $IMAGE
+docker push "$IMAGE"
 
 echo "image_name=$IMAGE" >> $GITHUB_OUTPUT
