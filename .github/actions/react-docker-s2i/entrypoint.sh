c8276f3a6a5dbad3b8c879272a4198d6907ea0c6
abdosaad203 <abdosaad4900@gmail.com>
2026-01-13 03:04:18 +0200

    Fix React S2I: build from github workspace

--- .github/actions/react-docker-s2i/entrypoint.sh
+++ .github/actions/react-docker-s2i/entrypoint.sh
@@ -7,17 +7,18 @@ echo "$INPUT_PASSWORD" | docker login "$INPUT_REGISTRY" \
 
 IMAGE="$INPUT_REGISTRY/$INPUT_IMAGE_NAME:$INPUT_TAG"
 
-echo "Listing frontend directory"
-ls -la frontend
-ls -la frontend/dist
+echo "Workspace content:"
+ls -la /github/workspace
+ls -la /github/workspace/frontend
+ls -la /github/workspace/frontend/dist
 
 echo "Build Docker image"
 docker build \
   -t "$IMAGE" \
   -f "$GITHUB_ACTION_PATH/Dockerfile.react" \
-  frontend
+  /github/workspace/frontend
 
-echo "Push image"
+echo "Push Docker image"
 docker push "$IMAGE"
 
 echo "image_name=$IMAGE" >> $GITHUB_OUTPUT
